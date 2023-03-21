30f6e20bd812bf9bf2b4d172a585d75f0496cd57
David Bolt <David.Bolt@Sfcknox.org>
2023-03-21 18:16:27 -0400

    work on sync

--- apps/sync/diff.js
+++ apps/sync/diff.js
@@ -1,29 +1,28 @@
 class diff {  //  diff - client-side
   /*
-  this client side code create lists of differences between laptop and desktop files
+  this client side code shows the  differences between two directors on differect computers
 
   Assume:
-  laptop mainifest file - contains all files in directory that we want to sync
-  desktop machine (home or office) - contains all files in directory
+   dir1 and diry2 maybe on same or seperate computers
+   The servers create mainifest files that contain a list of all files.  The lists are comppared and create sets:
 
-  Create:
-    laptop.json.tags.desktopMatch
-    laptop.json.tags.desktopMissing
-    desktop.json.index.lapTopMissing that starts with all rows
-    desktop.json.tags.lapTopMissing at end from desktop.json.index.lapTopMissing
+   dir1Only  - delete it from dir1 or copy to dir2 
+   Intersection- nothing or delete both
+   dir2Only  - delte it from dir2 or copy to dir1
 
   Process:
-      walk laptap, add to tags.desktopMatch or json.tags.desktopMissing
-      if file exists on desktop delete from index
-      ad end of process create
-  */
+      walk dir1 
+      if file is in dir2 add to intersection and remove from dir2 index
+      if not in Dir2 add to Dir1Only and remove from dir2 index
+      what is left in dir2 index becomes dir2Only
+*/
 
-
-constructor (//  diff - client-side
+constructor ( //  diff - client-side
 ) {
   this.i                    = 0;  // index into laptop data
   this.start;                     // init when diff main starts
 }
+
 
 async upload(   //  diff - client-side
 ) {  // upload from local machine to server, at end server should have same version of files as local
@@ -52,9 +51,7 @@ async upload(   //  diff - client-side
   await this.loadLocalServer(`client2server/upload/2-dir`     ,'2-dir-local');
   await this.loadLocalServer(`client2server/upload/3-links`   ,'3-links-local');
 
-
   // generate mainifest files on logedin server 
-
  msg = `{
     "server"      : "sync"
     ,"msg"        : "manifest"
@@ -70,24 +67,23 @@ async upload(   //  diff - client-side
     alert("Did not generated on logged in server");
     return;
   }
-  await this.loadRemoteServer(`client2server/1-manifest`,'1-manifest-remote');
-  await this.loadRemoteServer(`client2server/2-dir`     ,'2-dir-remote');
-  await this.loadRemoteServer(`client2server/3-links`   ,'3-links-remote');
+  await this.loadRemoteServer(`1-manifest`,'1-manifest-remote');
+  await this.loadRemoteServer(`2-dir`     ,'2-dir-remote');
+  await this.loadRemoteServer(`3-links`   ,'3-links-remote');
 
   // init local tags
   const local = app.db.getTable("1-manifest-local"); //
-  app.tableUx.tags.desktopMatch    = [];
-  app.tableUx.tags.desktopMissing = [];
+  app.tableUx.tags.localMatch   = [];
+  app.tableUx.tags.localMissing = [];
 
   // init remote tag
   const remote = app.db.getTable("1-manifest-remote");
-  desktop.json.tags.notOnLaptop =[]
 
   // init Desktop index
-  desktop.json.index = {}; // move to constructor is using index speeds things up
-  desktop.json.index.notOnLaptop = {};
-  for(let i=0; i<desktop.json.rows.length; i++) {
-    desktop.json.index.notOnLaptop[ desktop.json.rows[i][5]  ] = i;
+  remote.json.index = {}; // move to constructor is using index speeds things up
+  remote.json.index.notOnLocal =[]
+  for(let i=0; i<remote.json.rows.length; i++) {
+    remote.json.index.notOnLocal[ remote.json.rows[i][5]  ] = i;
   }
 
   this.walk();
@@ -97,6 +93,86 @@ async upload(   //  diff - client-side
   app.tableUx.display()
 }
 
+
+walk(  //  diff - client-side
+// keep calling walk until all files on dir1 have been checked to see if they live in dir2
+) {
+  const local  = app.db.getTable("1-manifest-local");
+  const r       =  local.json.rows;
+
+  const remote = app.db.getTable("1-manifest-remote");
+  const rr     = remote.json.rows;
+  const displayStatus = 10000;  // display status after this many rows are looked at
+
+  // walk local file list
+  for (let i=0; this.i < r.length && i < displayStatus; this.i++) {
+    i++; // loop counting
+    // use indexes
+    if (typeof(remote.json.index.notOnLocal[r[this.i][5]]) === 'number' ) {
+      // add to remoteMatch
+      laptop.json.tags.remoteMatch.push(this.i);
+      // delete from notOnLaptop
+      delete remote.json.index.notOnLaptop[r[this.i][5]];
+    } else {
+      // did not find match
+      //local.json.tags.remoteMissing.push(this.i);
+      app.tableUx.tags.localMissing.push(this.i);
+    }
+  };
+
+  // display status
+  document.getElementById("json").innerHTML = `
+  elasped time         = ${(new Date() - this.start)/1000}
+
+  locate file          = ${this.i} of ${r.length}
+  remote match        = ${app.tableUx.tags.localMatch.length  }
+  remote Missing      = ${app.tableUx.tags.localMissing.length}
+
+  Not on local        = ${remote.json.index.notOnLocal.length}
+     `
+//  Not on local        = ${Object.keys(remote.json.index.notOnLaptop.length)}
+  if (this.i <r.length) {
+    // more work todo, start again in 10 millisec
+    setTimeout( app.diff.walk.bind(this), 10 );
+  } else {
+    // done, create tag from indexe
+     Object.keys(desktop.json.index.notOnLaptop).forEach((key, index) => {
+        desktop.json.tags.notOnLaptop.push( desktop.json.index.notOnLaptop[key] );
+    });
+
+  }
+}
+
+
+async loadLocalServer( //  diff - client-side
+   fileName
+  ,tableName=fileName
+  ){
+  const table  = app.db.tableAdd(tableName);       // create table and add to db
+  const csv    = new csvClass(table);              // create instace of CSV object
+
+  // load csv file from synced desktop
+  const file   = await app.proxy.getText(`https://synergyalpha.sfcknox.org/sync/${fileName}.csv`);
+
+  csv.parseCSV(file, "json");         // parse loaded CSV file and put into table
+  app.db.displayMenu('menu', "app.displayTable(this)", "app.export()"); // display menu of tables, user can select one to display
+}
+
+
+async loadRemoteServer( //  diff - client-side
+  fileName
+  ,tableName=fileName
+  ){
+  const table  = app.db.tableAdd(tableName);       // create table and add to db
+  const csv    = new csvClass(table);              // create instace of CSV object
+
+  // load csv file from synced desktop
+  //const file   = await app.proxy.getText(`/users/sync/${this.machine}/${fileName}.csv`);
+  const file   = await app.proxy.getText(`/users/sync/client2server/${fileName}.csv`);
+
+  csv.parseCSV(file, "json");         // parse loaded CSV file and put into table
+  app.db.displayMenu('menu', "app.displayTable(this)", "app.export()"); // display menu of tables, user can select one to display
+}
 
 
 async main(   //  diff - client-side
@@ -149,84 +225,5 @@ async main(   //  diff - client-side
   //app.tableUx.display()
 }
 
-
-async loadLocalServer( //  diff - client-side
-   fileName
-  ,tableName=fileName
-  ){
-  const table  = app.db.tableAdd(tableName);       // create table and add to db
-  const csv    = new csvClass(table);              // create instace of CSV object
-
-  // load csv file from synced desktop
-  const file   = await app.proxy.getText(`https://synergyalpha.sfcknox.org/sync/${fileName}.csv`);
-
-  csv.parseCSV(file, "json");         // parse loaded CSV file and put into table
-  app.db.displayMenu('menu', "app.displayTable(this)", "app.export()"); // display menu of tables, user can select one to display
-}
-
-
-async loadRemoteServer( //  diff - client-side
-  fileName
-  ,tableName=fileName
-  ){
-  const table  = app.db.tableAdd(tableName);       // create table and add to db
-  const csv    = new csvClass(table);              // create instace of CSV object
-
-  // load csv file from synced desktop
-  //const file   = await app.proxy.getText(`/users/sync/${this.machine}/${fileName}.csv`);
-  const file   = await app.proxy.getText(`/users/sync/${fileName}.csv`);
-
-  csv.parseCSV(file, "json");         // parse loaded CSV file and put into table
-  app.db.displayMenu('menu', "app.displayTable(this)", "app.export()"); // display menu of tables, user can select one to display
-}
-
-
-walk(  //  diff - client-side
-// keep calling walkLaptop until all laptop files have been searched on desktop
-) {
-  const laptop  = app.db.getTable("laptop");
-  const r       =  laptop.json.rows;
-
-  const desktop = app.db.getTable("desktop");
-  const rr      = desktop.json.rows;
-  const displayStatus = 10000;
-
-  // walk laptop
-  for (let i=0; this.i < r.length && i < displayStatus; this.i++) {
-    i++; // loop counting
-    // use indexes
-    if (typeof(desktop.json.index.notOnLaptop[r[this.i][5]]) === 'number' ) {
-      // add to  desktopMatch
-      laptop.json.tags.desktopMatch.push(this.i);
-      // delete from notOnLaptop
-      delete desktop.json.index.notOnLaptop[r[this.i][5]];
-    } else {
-      // did not find match
-      laptop.json.tags.desktopMissing.push(this.i);
-    }
-  };
-
-  // display status
-  document.getElementById("json").innerHTML = `
-  elasped time         = ${(new Date() - this.start)/1000}
-
-  laptop file          = ${this.i} of ${r.length}
-  Desktop match        = ${laptop.json.tags.desktopMatch.length  }
-  Desktop Missing      = ${laptop.json.tags.desktopMissing.length}
-
-  Not on Laptop        = ${Object.keys(desktop.json.index.notOnLaptop).length}
-     `
-
-  if (this.i <r.length) {
-    // more work todo, start again in 10 millisec
-    setTimeout( app.diff.walk.bind(this), 10 );
-  } else {
-    // done, create tag from indexe
-     Object.keys(desktop.json.index.notOnLaptop).forEach((key, index) => {
-        desktop.json.tags.notOnLaptop.push( desktop.json.index.notOnLaptop[key] );
-    });
-
-  }
-}
 
 }//  diff - client-side  //////// end of cass
