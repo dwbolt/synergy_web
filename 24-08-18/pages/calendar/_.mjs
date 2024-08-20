1ac040ada8f0343ad3a0afa03746f89a8b09f6a9
David Bolt <David.Bolt@Sfcknox.org>
2024-08-20 15:12:46 -0400

    convert login to web component

---
+++ 24-08-18/pages/calendar/_.mjs
@@ -0,0 +1,22 @@
+// page module, exectued wwhen page finish loading
+import  {page_         }    from '/_lib/UX/page_.mjs'
+import  {calendar_class}    from '/_lib/web_componets/sfc-calendar/_.mjs';  // support <sfc-calendar> web componet
+
+class page_calendar  extends page_ { // sfcknox2/pages/clandar
+
+async display(){
+      super.display();              //
+      this.calendar = document.getElementById("calendar");
+      this.calendar.calendar_add("/users/databases/synergy/calendar"); // logged on users calender
+      await this.calendar.init();                                 // load calenders and create html place holders
+      await this.calendar.main();   // display calender or event page
+}
+
+
+}
+
+export {page_calendar};
+
+const page         = new page_calendar("calendar");
+app.pages.calendar =  page;
+await page.init();
